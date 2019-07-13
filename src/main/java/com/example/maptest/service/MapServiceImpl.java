3ed1739f609255087eb4a0be6fab1174154f0b85
archit_chandra <send2archit.chandra@gmail.com>
2019-07-13 13:55:52 +0200

    Used ConcurrentHashMap, CopyOnWriteArrayList to avoid ConcurrentModificationException

--- src/main/java/com/example/maptest/service/MapServiceImpl.java
+++ src/main/java/com/example/maptest/service/MapServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Logger;
 
 public class MapServiceImpl implements MapService {
@@ -13,16 +15,16 @@ public class MapServiceImpl implements MapService {
 
     @Override
     public Map<String, List<String>> getOriginalMap() {
-        Map<String, List<String>> map = new HashMap<>();
+        Map<String, List<String>> map = new ConcurrentHashMap<>();
 
         String personId1 = "P1";
-        List<String> personId1RequestIds = new ArrayList<>();
+        List<String> personId1RequestIds = new CopyOnWriteArrayList<>();
         personId1RequestIds.add("R1P1");
         personId1RequestIds.add("R2P1");
         map.put(personId1, personId1RequestIds);
 
         String personId2 = "P2";
-        List<String> personId2RequestIds = new ArrayList<>();
+        List<String> personId2RequestIds = new CopyOnWriteArrayList<>();
         personId2RequestIds.add("R1P2");
         personId2RequestIds.add("R2P2");
         personId2RequestIds.add("R3P2");
@@ -37,26 +39,36 @@ public class MapServiceImpl implements MapService {
         /*Map<String, List<String>> clonedMap = cloneMap(originalMap);*/
 
         // Worked
-        Map<String, List<String>> clonedMap = new HashMap<>();
+        /*Map<String, List<String>> clonedMap = new HashMap<>();
         originalMap.forEach((personId, requestIdList) -> {
             List<String> requestIds = new ArrayList<>(requestIdList);
             clonedMap.put(personId, requestIds);
-        });
+        });*/
 
 
-        // Not worked
-        /*ConcurrentMap<String, List<String>> clonedMap = new ConcurrentHashMap<>(originalMap);*/
+        // Not worked (No need, originalMap should be ConcurrentHashMap)
+        //Map<String, List<String>> clonedMap = new ConcurrentHashMap<>(originalMap);
 
         System.out.println("===========> Cloned Map");
         System.out.println("===========> Deleting requests");
 
-        for (Map.Entry<String, List<String>> entry : clonedMap.entrySet()) {
+        // Worked
+        for (Map.Entry<String, List<String>> entry : originalMap.entrySet()) {
             for (String requestId : entry.getValue()) {
                 List<String> requests = originalMap.get(entry.getKey());
                 requests.remove(requestId);
-                originalMap.put(entry.getKey(), requests);
+                //originalMap.put(entry.getKey(), requests);
             }
         }
+
+        // Not worked
+        /*for (Map.Entry<String, List<String>> entry : originalMap.entrySet()) {
+            for (String requestId : entry.getValue()) {
+                List<String> requests = originalMap.get(entry.getKey());
+                requests.remove(requestId);
+                //originalMap.put(entry.getKey(), requests);
+            }
+        }*/
 
         // Not worked
         /*Iterator<Map.Entry<String, List<String>>> mapIterator = originalMap.entrySet().iterator();
@@ -69,6 +81,17 @@ public class MapServiceImpl implements MapService {
             }
 
         }*/
+
+        // Not worked
+        /*Iterator<Map.Entry<String, List<String>>> mapIterator = originalMap.entrySet().iterator();
+        while (mapIterator.hasNext()) {
+            Map.Entry<String, List<String>> entry = mapIterator.next();
+            List<String> requestIdList = originalMap.get(entry.getKey());
+            for (String requestId : entry.getValue()) {
+                requestIdList.remove(requestId);
+            }
+        }*/
+
 
         System.out.println("===========> Deleted requests");
     }
